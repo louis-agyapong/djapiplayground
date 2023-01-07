ce5d186cd4b200bf7b9ee6dc22b95ad86f2a4682
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-01-07 00:16:56 +0000

    poll models

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(_("Question"), max_length=100)
+    created_by = models.ForeignKey(User, verbose_name=_("Created By"), on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(_("Published Date"), auto_now=True)
+
+    def __str__(self) -> str:
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey("Poll", verbose_name=_(""), on_delete=models.CASCADE)
+    choice_text = models.CharField(_("Choice Text"), max_length=100)
+
+    def __str__(self) -> str:
+        return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey("Choice", verbose_name=_("Choice"), on_delete=models.CASCADE)
+    poll = models.ForeignKey("Poll", verbose_name=_("Poll"), on_delete=models.CASCADE)
+    votes = models.IntegerField(_("Votes"), default=0)
+    voted_by = models.ForeignKey(User, verbose_name=_("Voted By"), on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(name="unique_poll_voted_by", fields=["poll", "voted_by"]),
+        ]
