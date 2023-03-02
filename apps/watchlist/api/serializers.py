4bb7d92010629c0d7b445aa05dcabdd7d66a82e6
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-03-02 21:00:33 +0000

    Review Serializer

--- apps/watchlist/api/serializers.py
+++ apps/watchlist/api/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 
-from apps.watchlist.models import Movie, StreamingPlatform
+from apps.watchlist.models import Movie, Review, StreamingPlatform
 
 # def name_length(value):
 #     if len(value) < 2:
 #         raise serializers.ValidationError("Name is too short!")
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
     title_length = serializers.SerializerMethodField()
 
     class Meta:
